9d3307f71a4c4fe8f5fd684ab061cbfccf645398
player62 <uri42012w@naver.com>
2022-11-08 17:36:05 +0900

    Update login2_NJW.py

--- login2_NJW.py
+++ login2_NJW.py
@@ -15,7 +15,7 @@ import numpy as np
 # ====================================================================================================
 def suppress_qt_warnings():
     environ["QT_DEVICE_PIXEL_RATIO"] = "0"
-    #environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     environ["QT_SCREEN_SCALE_FACTORS"] = "1"
     environ["QT_SCALE_FACTOR"] = "1"
 # ====================================================================================================
